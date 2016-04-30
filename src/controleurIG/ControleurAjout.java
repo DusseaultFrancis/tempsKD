fa0b3cbeb8779835c184a6adce8bf565539a0f2b
DusseaultFrancis <francisdusseault1@gmail.com>
2016-04-30 11:08:41 -0400

    Modification bouton FenetrePrincipale et actionlistener sur AjoutClient.

--- src/controleurIG/ControleurAjout.java
+++ src/controleurIG/ControleurAjout.java
@@ -12,58 +12,73 @@ import static json.Ecriture.creationObjetJson;
 import regleAffaire.Client;
 import regleAffaire.ListeClient;
 
+
 public class ControleurAjout implements ActionListener {
+	
+	
+	private ListeClient modele;
+	private AjoutClient ajout;
 
-    private ListeClient modele;
-    private AjoutClient ajout;
+	List<Client> listeClient = new ArrayList<Client>();
+	
+	// Constructeur pour FenetreAjout
+	public ControleurAjout(AjoutClient fenetreAjout) {
+		this.modele = new ListeClient();
+		ajout = fenetreAjout;
+		
+	}
+	
 
-    List<Client> listeClient = new ArrayList<Client>();
 
-    // Constructeur pour FenetreAjout
-    public ControleurAjout(AjoutClient fenetreAjout) {
-        this.modele = new ListeClient();
-        ajout = fenetreAjout;
+	// return modele
+	public ListeClient getModele(){
+		return modele;
+	}
 
-    }
+	
+	@Override
+	public void actionPerformed(ActionEvent event) {
 
-    // return modele
-    public ListeClient getModele() {
-        return modele;
-    }
+		// Obtenir la source de l'évenement
+		Object source = event.getSource();
+		
+		
+		if(source == ajout.getBoutonSauvegarder()){ 
+			
+			String nom = ajout.getNom().getText();
+			String prenom = ajout.getPrenom().getText();
+			String entreprise = ajout.getEntreprise().getText();
+			String telephone = ajout.getTelephone().getText();
+			String adresse = ajout.getAdresse().getText();
+                        String courriel = ajout.getCourriel().getText();
 
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        // Obtenir la source de l'évenement
-        Object source = event.getSource();
+			Client client = new Client(nom,prenom,entreprise,telephone,adresse,courriel);
+                        
+			modele.ajouterClient(client);
+                        
+			ajout.dispose();
+                        
+                        
+                        // Lire le fichier, le mettre dans une listeClient. Ensuite,
+                        // ajouter le client a la liste et ecrire dans le fichier.
+                        
+                        
+                        
+                    try {
+                        creationObjetJson(client, "Liste des clients.json");
+                    } catch (Exception ex) {
+                        Logger.getLogger(ControleurAjout.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+			
 
-        if (source == ajout.getBoutonSauvegarder()) {
+		}else if (source==ajout.getBoutonFermer()){  
+			
+			ajout.dispose();
 
-            String nom = ajout.getNom().getText();
-            String prenom = ajout.getPrenom().getText();
-            String entreprise = ajout.getEntreprise().getText();
-            String telephone = ajout.getTelephone().getText();
-            String adresse = ajout.getAdresse().getText();
-            String courriel = ajout.getCourriel().getText();
-
-            Client client = new Client(nom, prenom, entreprise, telephone, adresse, courriel);
-
-            modele.ajouterClient(client);
-
-            ajout.dispose();
-
-            // Lire le fichier, le mettre dans une listeClient. Ensuite,
-            // ajouter le client a la liste et ecrire dans le fichier.
-            try {
-                creationObjetJson(client, "Liste des clients.json");
-            } catch (Exception ex) {
-                Logger.getLogger(ControleurAjout.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        } else if (source == ajout.getBoutonFermer()) {
-
-            ajout.dispose();
-
-        }
-    }
+		}
+	}
+	
 
 }
+		
+	
